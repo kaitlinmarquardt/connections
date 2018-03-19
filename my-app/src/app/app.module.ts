288dd7b40018bdc6d941cd54f3e961e693a5b9a4
kaitlinmarquardt <kaitlinmarquardt@gmail.com>
2018-03-19 15:01:17 -0500

    Added tables component, tables NG module and material icons

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTable } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatTable
+  ],
+  exports: [
+    MatTable
   ],
   providers: [],
   bootstrap: [AppComponent]
